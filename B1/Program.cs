f6e445e641d74242835858bfd3acc6fe04eeb533
KeyNoirr <key.garena@gmail.com>
2018-03-09 17:23:43 +0700

    Add project files.

---
+++ B1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hn = Dns.GetHostName();
+            Console.Write("Host Name: ", hn);
+            IPHostEntry iphe = Dns.Resolve(hn);
+            Console.WriteLine(iphe.HostName);
+            foreach (IPAddress s in iphe.AddressList)
+            {
+                Console.WriteLine("IP: {0}", s);
+            }
+        }
+    }
+}
---
+++ B2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap Domain Hoac IP: ");
+            string hn = Console.ReadLine();
+            IPHostEntry iphe = Dns.Resolve(hn);
+            Console.WriteLine("Host Name: {0}", iphe.HostName);
+            foreach (IPAddress s in iphe.AddressList)
+            {
+                Console.WriteLine("IP: {0}", s);
+            }
+        }
+    }
+}
